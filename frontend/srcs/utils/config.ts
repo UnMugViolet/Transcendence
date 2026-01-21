901c4374edc4d7b996ecd7969045e2f3bf1ff0ed
Célin <celine.louaintier@gmail.com>
2026-01-21 12:14:21 +0100

    localhost working in prod and dev but distance connection only in prod

--- frontend/srcs/utils/config.ts
+++ frontend/srcs/utils/config.ts
@@ -1,3 +1,14 @@
-export const BACKEND_URL = window.location.hostname === 'localhost'
-    ? 'http://localhost:3000'
-    : '/api';
+export const BACKEND_URL = (() => {
+  const hostname = window.location.hostname;
+  const protocol = window.location.protocol;
+
+  if (hostname === 'localhost') {
+    if (protocol === 'http:') {
+      return 'http://localhost:3000';
+    }
+  }
+  return '/api';
+})();
+
+// export const BACKEND_URL = '/api';
+
